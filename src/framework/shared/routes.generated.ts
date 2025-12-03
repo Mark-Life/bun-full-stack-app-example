a4ea33167df77e8b3b4794b4267e9d09cc158667
Mark-Life <108@mark-life.com>
2025-12-03 16:13:21 +0200

    chore: move out of a folder to stop having problems with deploys

--- src/framework/shared/routes.generated.ts
+++ src/framework/shared/routes.generated.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
-// Generated at: 2025-12-03T14:00:48.755Z
+// Generated at: 2025-12-03T14:12:09.782Z
 
 /**
  * Union type of all valid route paths in the application.
